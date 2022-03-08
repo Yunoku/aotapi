421365066c6cc65443658828ff4bed01213262cb
Dylan Beurskens <info@dylanbeurskens.com>
2022-03-08 11:58:01 +0100

    Added police

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -8,35 +8,47 @@ class Characters {
         this.characterDiv = characterDiv;
     }
 
+    static #loopCharacters(name, characterCollection, imageCollection) {
+        let characterImage = imageCollection[characterCollection.indexOf(name)];
+        let image = document.createElement('img');
+        let characterFigure = document.createElement('figure');
+        let characterFigcaption =  document.createElement('figcaption');
+
+        this.characterDiv.append(characterFigure);
+        characterFigure.appendChild(image);
+        document.querySelectorAll('img')[characterCollection.indexOf(name)].src = `../images/${characterImage}`;
+        characterFigure.appendChild(characterFigcaption);
+        document.querySelectorAll('figcaption')[characterCollection.indexOf(name)].innerHTML = name;
+    }
+
     static getAllScouts() {
         this.characters.then(characters => {
             for(const name of characters.scouts.names) {
-                let characterImage = characters.scouts.images[characters.scouts.names.indexOf(name)];
-                let image = document.createElement('img');
-                let characterFigure = document.createElement('figure');
-                let characterFigcaption =  document.createElement('figcaption');
-
-                this.characterDiv.append(characterFigure);
-                characterFigure.appendChild(image);
-                document.querySelectorAll('img')[characters.scouts.names.indexOf(name)].src = `../images/${characterImage}`;
-                characterFigure.appendChild(characterFigcaption);
-                document.querySelectorAll('figcaption')[characters.scouts.names.indexOf(name)].innerHTML = name;
+                this.#loopCharacters(name, characters.scouts.names, characters.scouts.images);
             }
         });
     }
 
-    static getSingleScout(givenName) {
+    static getAllPolice() {
         this.characters.then(characters => {
-            for(const name of characters.scouts.names) {
-                if(name == givenName) {
-                    console.log(name);
-                } else {
-                    console.error("The requested character does not exist");
-                    continue;
-                }
+            for(const name of characters.military_police.names) {
+                this.#loopCharacters(name, characters.military_police.names, characters.military_police.images);
             }
         });
     }
+    
+    // static getSingleScout(givenName) {
+    //     this.characters.then(characters => {
+    //         for(const name of characters.scouts.names) {
+    //             if(name == givenName) {
+    //                 console.log(name);
+    //             } else {
+    //                 console.error("The requested character does not exist");
+    //                 continue;
+    //             }
+    //         }
+    //     });
+    // }
 
     static setCharacters(characters) {
         this.characters = characters;
