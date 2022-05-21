f65ea3c666e1aed040a7e9ea0cd70a6860fa3d0d
Dylan Beurskens <info@dylanbeurskens.com>
2022-05-21 10:23:40 +0200

    Optimized fetch data

--- public/scripts/data.js
+++ public/scripts/data.js
@@ -4,8 +4,7 @@ export let characterObject; // This variable contains the fetched data
 // This function gets the data from the aot.json file using Fetch
 async function getCharacterData() {
     const response = await fetch('aot.json');
-    const characters = await response.json();
-    return characters;
+    return await response.json();
 }
 
 // This variable pushes data into the characterObject variable 
